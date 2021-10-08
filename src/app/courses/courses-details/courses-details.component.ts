729eed6310a64b175e5ae8920debffb943cd9bc8
othman-avm <othman.abdulsalam@avmsolutionsuk.com>
2021-10-08 11:06:06 +0100

    isolated state mutations at component level

--- src/app/courses/courses-details/courses-details.component.ts
+++ src/app/courses/courses-details/courses-details.component.ts
@@ -5,17 +5,31 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './courses-details.component.html',
   styleUrls: ['./courses-details.component.scss']
 })
-export class CoursesDetailsComponent implements OnInit
+export class CoursesDetailsComponent
 {
 
-  @Input() course;
+
+  selectedCourse;
+  originalTitle;
+
+  @Input() set course(value)
+  {
+    if (value)
+    {
+      // create clone of course variable
+      // avoid shared mutability where we dont want it
+      // shouldnt affect title displayed in course-details until
+      // after submit is clicked
+      this.selectedCourse = Object.assign({}, value)
+      this.originalTitle = value.title;
+    }
+  };
+
+
   @Output() saved = new EventEmitter();
 
   @Output() cancelled = new EventEmitter();
-  constructor() { }
 
-  ngOnInit(): void
-  {
-  }
+
 
 }
